39d7b95fdab77cd4c0955145d1134912b86e71e7
Paul Mikesell2 <pmikesell2@gmail.com>
2014-02-20 10:01:43 -0800

    Added test comment.

--- wprox.js
+++ wprox.js
@@ -4,6 +4,7 @@ var prox_con = require('./proxy_connector')
 
 /* main loop */
 function main() {
+    /* Create the main proxy listener */
     pcon = new prox_con.ProxyConnector();
     s = http.createServer(prox_con.method(pcon, 'handleClientRequest'));
     s.listen(2004)
